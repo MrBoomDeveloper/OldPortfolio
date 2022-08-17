89d7bb3468a512a67b09e2b87e16eb33302593d0
MrBoomDeveloper <mrboomdev@yahoo.com>
2022-08-18 01:32:31 +0500

    добавил sass, так-как лоадеры вебпака на андроиде не работают ._.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
-  },
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'docs/js'),
+		clean: true
+	},
+	mode: 'development'
 };
