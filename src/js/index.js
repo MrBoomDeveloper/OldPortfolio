c2eaf42c4984c24007cc0a96155eaf2455ea5e28
MrBoomDeveloper <mrboomdev@yahoo.com>
2022-08-31 14:53:06 +0500

    знаете? это довольно-таки одновременно и весело и дерьмово пытаться починить эти два бага. вроде когда я дебажу через localhost, то все ок, но стоит закомитить код в гитчаб, то баги из ни откуда появляются. и так каждый раз! Вроде кажется что все тк, можно идти смотреть аниме, но опять! и в итоге я раз за разом пытаюсь все починить. аообще я планировал доделать все к 1 сентября, но в итоге все вот так.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 import { el } from "boomutil";
 import "boomutil/js/views";
 import "boomutil/js/events";
+import { fadeHeader } from "./layout/header";
 import "./views";
+
+window.addEventListener("load", () => {
+	const app = document.body;
+	app.addEventListener("scroll", () => {
+		fadeHeader(app);
+	});
+});
  
 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(89331187, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true, ecommerce:"app" });
