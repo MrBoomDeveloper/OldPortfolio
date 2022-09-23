740953f65bb464bdf1e90322d53720cbc1eb4af1
MrBoomDeveloper <mrboomdev@yahoo.com>
2022-09-23 20:16:51 +0500

    добавил текст в обо мне, обновил компонент большой кнопки

--- src/js/home.js
+++ src/js/home.js
@@ -3,46 +3,13 @@ import { el, notify, reveal, initReveal } from "boomutil";
 import { initHeader } from "./layout/header";
 
 initHeader(window, true);
-setTimeout(() => initReveal(window), 250);
+setTimeout(() => initReveal(window, 125), 250);
 
-/*(async () => {
-	const projects = await import("../json/projects");
-	console.log(projects);
-})();
-
-class BoomFilter extends HTMLElement {
-	//Это все я написал!? #*@*#!? Это прям ужас. Нужно будет все переписать тут
-	connectedCallback() {
-		this.render();
-	}
-	
-	render() {
-		let html = "";
-		if(!this.cats) return;
-		try {
-			for(const cat of this.cats) {
-				html+= `<boom-item title="${cat}" type="${this.getAttribute('type')}" class="reveal"></boom-item>`;
-			}
-		} catch(exception) {
-			console.error(exception + ".........");
-		}
-		this.innerHTML = html;
-	}
-	
-	set array(arr) {
-		this.cats = arr;
-		this.render();
-	}
-	
-	get array() {
-		return this.cats;
-	}
-}
-
-customElements.define("boom-filter", BoomFilter);
-
-window.onload = () => {
-	el("#tags").array = ["test", "test1", "test2"];
-}*/
+el("#filter").load = [
+	{ title: "Java" },
+	{ title: "CSS" },
+	{ title: "HTML5" },
+	{ title: "FireBase" }
+]
 
 
